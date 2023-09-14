d7dc5c2437fc467b6def6eb4a8c36c1063560855
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-09-14 16:36:33 +0700

    udpate

--- src/main/java/com/example/sd18102/controller/AppController.java
+++ src/main/java/com/example/sd18102/controller/AppController.java
@@ -4,14 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/sinh-vien")
 public class AppController {
 
     @GetMapping("/hien-thi")
-//    @RequestMapping(value = "/hien-thi", method = RequestMethod.GET)
+    @RequestMapping(value = "/hien-thi", method = RequestMethod.GET)
     public String hienThi(){
+        return "index";
+    }
+
+    @GetMapping("/chi-tiet")
+    public String chiTiet(
+            @RequestParam("id") String ma
+
+    ){
+        System.out.println("id la: "+ma);
         return "index";
     }
 }
