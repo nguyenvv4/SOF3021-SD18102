915b93d5246d0431c2a98a17b0c17060fd5f060a
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-09-27 08:18:21 +0700

    udpate

--- src/main/java/com/example/sd18102/model/CuaHang.java
+++ src/main/java/com/example/sd18102/model/CuaHang.java
@@ -1,14 +1,17 @@
 package com.example.sd18102.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
 @Table(name = "cua_hang")
 @Entity
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class CuaHang {
 
     @Id
--- src/main/java/com/example/sd18102/repository/CuaHangRepository.java
+++ src/main/java/com/example/sd18102/repository/CuaHangRepository.java
@@ -2,10 +2,18 @@ package com.example.sd18102.repository;
 
 import com.example.sd18102.model.CuaHang;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CuaHangRepository extends JpaRepository<CuaHang, UUID> {
+
+    List<CuaHang> findByTenAndMaOrderByMaDesc(String ten, String ma);
+
+
+    @Query(value = "select * from cua_hang", nativeQuery = true)
+    List<CuaHang> getAll();
 }
